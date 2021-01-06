5420c8e67341d5d27713783dd4a6c72291de1b5d
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-01-06 18:23:00 +0100

    Create cp backup script (incremental, full snapshots, hard links)
    * 2018-09-23: First idea and design of the backup mechanism
    * 2020-10-28: First draft of the implementation (untested)
    * 2020-11-07: Live logging to console and file without line buffer
    * 2020-11-08: Parsing of script arguments
    * 2021-01-06: Final tests and code clean-up

---
+++ backup-cp-inc-snapshot-hardlink.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Incremental file-based backup that provides full snapshots with hard links
+
+if [ $# -lt 2 ]
+then
+	printf "Usage: $0 sourcePath targetPath [sourceDirs...]\n"
+	exit 1
+fi
+
+# configuration of source and target directories
+sourcePath="$1"
+targetPath="$2"
+sourceDirs=(".")
+
+shift 2
+if [ $# -gt 0 ]
+then
+	sourceDirs=("$@")
+fi
+
+# create the directory for the latest backup if not already existing
+backupDir="$targetPath/latest"
+mkdir -p "$backupDir"
+
+# create a new directory for the current snapshot
+snapshotDate=$(date +%F)
+# snapshotDate=$(date "+%F %H-%M") # for testing
+snapshotDir="$targetPath/$snapshotDate"
+mkdir -p "$snapshotDir"
+logFile="$snapshotDir/backup.log"
+
+# write stdout and stderr of the given command to a logfile too
+log() {
+	"$@" |& tee -a "$logFile"
+}
+
+# try to line buffer output to see live updates despite the usage of a pipe
+# https://stackoverflow.com/questions/42938106/modifying-tee-a-out-txt-to-stream-output-live-rather-than-on-completio
+if hash unbuffer 2> /dev/null
+then
+	log() {
+		unbuffer "$@" |& tee -a "$logFile"
+	}
+elif hash stdbuf 2> /dev/null
+then
+	log() {
+		stdbuf -oL "$@" |& tee -a "$logFile"
+	}
+fi
+
+# log the given line with a leading timestamp
+logtime() {
+	log printf "\n[$(date +%T)] $1\n"
+}
+
+# only write log header once for each snapshot
+if [ ! -f "$logFile" ]
+then
+	log printf "Backup of '$sourcePath' -- Snapshot from $(date +%x)\n"
+fi
+
+# copy files that have been created/updated since the last backup
+for dir in "${sourceDirs[@]}"
+do
+	logtime "Copying new and changed files from '$dir'"
+	log cp -avu "$sourcePath/$dir" "$backupDir"
+	# -u update files only (do not remove and re-copy files, this destroys the hard links!), without deletion, alternative:
+	# log rsync -avhh --delete "$sourcePath/$dir" "$backupDir"
+done
+# 4.9GiB: cp 42,941s / rsync 50,974s/44,782s
+
+# create the new snapshot using hard links
+logtime "Creating snapshot '$snapshotDir'"
+cp -al "$backupDir/." "$snapshotDir"
