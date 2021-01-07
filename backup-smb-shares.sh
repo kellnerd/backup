b224c0ee2299a4f65c3f04cb451388b622480150
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-01-07 15:24:00 +0100

    Add wrapper script to backup shares of an SMB server
    * Configure paths, SMB user and ask for the password
    * Mount all required SMB shares before performing the backup
    * Unmount all previously mounted SMB shares after the backup has finished

---
+++ backup-smb-shares.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+printf "Working directory: "
+pwd
+
+# SMB server and shares
+remote="//synology-ds115"
+shares=("video" "music" "audio" "web")
+user="backup"
+mountPath="/mnt/synology-ds115"
+
+printf "Password for $user@$remote: "
+read -s password
+echo
+
+# source and target directories
+sourcePath="$mountPath"
+targetPath="."
+
+# mount all required SMB shares
+for share in "${shares[@]}"
+do
+	echo "Mounting '$remote/$share' under '$mountPath/$share'"
+	PASSWD="$password" sudo -E mount.cifs "$remote/$share" "$mountPath/$share" -o user=backup
+done
+
+# perform an incremental backup for all shares
+./backup-rsync-inc-snapshot-hardlink.sh "$sourcePath" "$targetPath" "${shares[@]}"
+
+# unmount all previously mounted SMB shares
+for share in "${shares[@]}"
+do
+	echo "Unmounting '$mountPath/$share'"
+	sudo umount "$mountPath/$share"
+done
+
+read -p "Finished! Press enter to continue..."
